127d688bd25432f62040f5327eab6faa25025794
bahayg <bahayg@gmail.com>
2020-02-02 14:44:54 -0800

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 
 import NavBar from './NavBar'
 
+import GifListContainer from '../containers/GifListContainer'
+
+// My GIF API Key: ifokhatyRwHBc4OVWVnbgfbSxnEGfzg6
+
 // the App component should render out the GifListContainer component 
 
 const App = () => {
   return (
     <div>
         < NavBar color='black' title="Giphy Search" />
+        <GifListContainer />
     </div>
   )
 }
