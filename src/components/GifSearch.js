b71220161d1ae87844f51dee0451838f6bc53602
jmknopf1007 <jmknopf1007@gmail.com>
2020-02-06 10:31:20 -0800

    Done.

---
+++ src/components/GifSearch.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+
+class GifSearch extends Component {
+  constructor() {
+    super()
+    this.state={
+      userInput: ""
+    }
+  }
+
+  handleOnChange = (e) => {
+   this.setState({userInput: e.target.value})
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.onSubmitInput(this.state.userInput)
+  }
+
+  render() {
+    return(
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.userInput} onChange={this.handleOnChange}></input>
+        <button>Find Gifs</button>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default GifSearch
